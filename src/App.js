e3e89fe7d667ba04ee4f9cc2a34792eaecea9145
DanteThuta <dantethuta97@gmail.com>
2021-08-21 15:06:49 +0630

    Fetching Process and Create Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from 'react'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
-function App() {
+const App = ({onAdd}) => {
+    const [showAddTask,setshowAddTask] = useState(true)
+  // useState for putting the array into Task together
+  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+
+      console.log(data)
+    }
+    fetchTasks()
+  },[])
+
+  // Add Task
+  const addTask = (task) => {
+    // console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1;
+    // console.log(id);
+    const newTask = { id, ...task }
+    setTasks([...tasks ,newTask])
+  }
+  
+  //deleteing Tasks 1 by 1
+  const deleteTask = (id) => {
+    // console.log('delete',id);
+    setTasks(tasks.filter((task)=>task.id !== id))
+  }
+
+  // Progress to toggle the Reminder
+  const toggleReminder = (id) => {
+    // console.log(id);
+    setTasks(
+      //not Filter , require Map
+      tasks.map((task) =>
+    task.id ===id ? {...task, reminder:!task.reminder} : task
+    )
+    )
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container ">
+      <Header onAdd={() => setshowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ?
+        <Tasks tasks={tasks} onDelete={deleteTask}
+          onToggle={toggleReminder}  /> : 'No Tasks Found'}
+      
     </div>
-  );
+    
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types'
+import Button from '../components/Button'
+
+
+
+const Header = ({ title,onAdd,showAdd }) => {
+    // const onClick = () => {
+    //     console.log('click');
+    //}
+    
+    return (
+        <header className="header">
+            
+                <h1>{title}</h1>
+            <Button color={showAdd ? 'red' : 'green'}
+                text={showAdd ? 'Close' : 'Add'}
+                onClick={onAdd} />
+                
+        </header> 
+    )
+}
+
+Header.defaultProps = {
+    title:"Task Tracker"
+}
+
+Header.propTypes = {
+    title : PropTypes.string.isRequired,
+}
+
+// const HeadingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
+
+export default Header

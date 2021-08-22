61aaee2ec13bd2aed65962088d75854b104ff80d
DanteThuta <dantethuta97@gmail.com>
2021-08-22 17:07:45 +0630

    Fetching to&from Server and Routing Basic Complete

--- src/App.js
+++ src/App.js
@@ -1,58 +1,117 @@
 import {useState,useEffect} from 'react'
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
 
 const App = ({onAdd}) => {
     const [showAddTask,setshowAddTask] = useState(true)
   // useState for putting the array into Task together
   const [tasks, setTasks] = useState([])
   useEffect(() => {
+
+    // calling back data from the Server
+    const getTasks = async()=>{
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  },[])
+    //Fetching Task to call the Datas
     const fetchTasks = async () => {
       const res = await fetch('http://localhost:5000/tasks')
       const data = await res.json()
 
-      console.log(data)
+      // console.log(data)
+      return data;
     }
-    fetchTasks()
-  },[])
+
+    //Fetching Task to call Single Data
+    const fetchTask = async (id) => {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const data = await res.json()
+
+      // console.log(data)
+      return data;
+    }
+ 
 
   // Add Task
-  const addTask = (task) => {
+  const addTask = async(task) => {
+    
+    const res = await fetch('http://localhost:5000/tasks',{
+      method: 'POST',
+      headers: {
+        'Content-type' : 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+      const data = await res.json();
+
+      setTasks([...tasks,data])
     // console.log(task);
-    const id = Math.floor(Math.random() * 10000) + 1;
-    // console.log(id);
-    const newTask = { id, ...task }
-    setTasks([...tasks ,newTask])
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // // console.log(id);
+    // const newTask = { id, ...task }
+    // setTasks([...tasks ,newTask])
   }
   
   //deleteing Tasks 1 by 1
-  const deleteTask = (id) => {
+  const deleteTask = async(id) => {
+    //deleting from the Server
+    await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'DELETE',
+    })
     // console.log('delete',id);
     setTasks(tasks.filter((task)=>task.id !== id))
   }
 
   // Progress to toggle the Reminder
-  const toggleReminder = (id) => {
-    // console.log(id);
+  const toggleReminder = async(id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle,
+    reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'PUT',
+      headers: {
+        'Content-type' : 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    })
+    const data = await res.json()
+
+
     setTasks(
       //not Filter , require Map
       tasks.map((task) =>
-    task.id ===id ? {...task, reminder:!task.reminder} : task
+    task.id ===id ? {...task, reminder:data.reminder} : task
     )
     )
   }
   
   return (
+    <Router>
     <div className="container ">
       <Header onAdd={() => setshowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ?
-        <Tasks tasks={tasks} onDelete={deleteTask}
-          onToggle={toggleReminder}  /> : 'No Tasks Found'}
+     
       
+      <Route path='/' exact render={(props)=> (
+        <>
+           {showAddTask && <AddTask onAdd={addTask} />}
+           {tasks.length > 0 ?
+           <Tasks tasks={tasks} onDelete={deleteTask}
+          onToggle={toggleReminder}  /> : 'No Tasks Found'}
+        </>
+      )
+      } />
+      <Route path='/about' component={About} />
+      <Footer />
     </div>
-    
+    </Router>
   )
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyrighted &copy; 2021</p>
+            <Link to='/about'>About</Link>
+        </footer>
+    )
+}
+
+export default Footer
+
+
